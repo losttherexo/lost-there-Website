b3a08619e1f0d5faecd2da73c2712f62b6098d7f
André Vargas Roo <a.vargas252@icloud.com>
2023-07-11 12:25:49 -0400

    htmlFor fix

--- client/src/components/Newsletter.js
+++ client/src/components/Newsletter.js
@@ -16,12 +16,12 @@ const Newsletter = ({isOpen, handleNewsletter}) => {
     return (
         <div className='fixed self-center max-w-[75%] bg-white p-4 rounded'>
             <form onSubmit={handleSubmit} className='flex flex-col'>
-                <label for='email' className='font-medium'>Email</label>
+                <label htmlFor='email' className='font-medium'>Email</label>
                 <input placeholder='Type your email' name='email' className='my-2 border rounded-sm' />
                 <p className='font-medium'>I consent to being contacted by email</p>
                 <div className=''>
                     <input onClick={() => setConsent(!consent)} type='checkbox' name='consent' />
-                    <label for='consent' className='px-2'>Yes</label>
+                    <label htmlFor='consent' className='px-2'>Yes</label>
                 </div>
                 <button type='submit' className='bg-lime-500 rounded-md my-2 max-w-[25%] px-2 self-center'>Submit</button>
             </form>
