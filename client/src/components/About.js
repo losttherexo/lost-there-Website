934d4b2ecd429c54a3b630574821eae29e876e47
André Vargas Roo <a.vargas252@icloud.com>
2023-07-11 11:15:10 -0400

    quick update to about me

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,7 +2,7 @@ const About = () => {
     return (
         <div>
             <div className='flex justify-center py-3'>
-                <p className='text-lime-500'>hello fuckers</p>
+                <p className='text-lime-500'>this is about me</p>
             </div>
         </div>
     )
