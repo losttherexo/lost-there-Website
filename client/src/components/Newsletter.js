2f1d2c5a43095b1f9de080a51b935045b79ec5a8
André Vargas Roo <a.vargas252@icloud.com>
2023-07-11 12:07:31 -0400

    consent state implemented

--- client/src/components/Newsletter.js
+++ client/src/components/Newsletter.js
@@ -1,16 +1,24 @@
-const Newsletter = () => {
+import { useState } from "react"
 
+const Newsletter = () => {
+    const [consent, setConsent] = useState(false)
+    
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log(e.target.value)
     }
 
     return (
-        <div className='self-center max-w-[30%] bg-white p-4 rounded'>
+        <div className='self-center max-w-[75%] bg-white p-4 rounded'>
             <form onSubmit={handleSubmit} className='flex flex-col'>
-                <p className=''>Email</p>
+                <label for='email' className='font-medium'>Email</label>
                 <input placeholder='Type your email' name='email' className='my-2 border' />
-                <input type='submit' className='bg-lime-500 rounded-md my-2' />
+                <p className='font-medium'>I consent to being contacted by email</p>
+                <div className=''>
+                    <input onClick={() => setConsent(!consent)} type='checkbox' name='consent' />
+                    <label for='consent' className='px-2'>Yes</label>
+                </div>
+                <button type='submit' className='bg-lime-500 rounded-md my-2 max-w-[25%] px-2 self-center'>Submit</button>
             </form>
         </div>
     )
