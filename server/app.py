dc5dc0c2b067f6323a9d848d4de844b2edb3a3cd
Andre Vargas Roo <a.vargas252@icloud.com>
2023-07-13 15:34:25 -0400

    flask app instantiated

--- server/app.py
+++ server/app.py
@@ -1,1 +1,13 @@
 from flask import request, make_response, session, abort
+from flask_restful import Resource
+
+from config import app, db, api
+
+class Home(Resource):
+    def get(self):
+        return 'how did i get here?'
+
+api.add_resource(Home, '/')
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
