7e4ffd414c4387b75f8c3ea54749dc5b8a419155
Andre Vargas Roo <a.vargas252@icloud.com>
2023-07-14 10:57:35 -0400

    create db

--- server/app.py
+++ server/app.py
@@ -1,13 +1,7 @@
-from flask import request, make_response, session, abort
+from flask import request
 from flask_restful import Resource
 
 from config import app, db, api
-
-class Home(Resource):
-    def get(self):
-        return 'how did i get here?'
-
-api.add_resource(Home, '/')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/config.py
+++ server/config.py
@@ -1,17 +1,11 @@
-import os
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from flask_bcrypt import Bcrypt
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lostthere.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
@@ -24,7 +18,5 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
-
-bcrypt = Bcrypt(app)
 
 CORS(app)
