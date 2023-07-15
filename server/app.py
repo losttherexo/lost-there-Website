4796fe46c98c5f6afa6a958a460f8f139aa9e1de
André Vargas Roo <a.vargas252@icloud.com>
2023-07-15 11:46:14 -0400

    Shows added to API

--- server/app.py
+++ server/app.py
@@ -2,14 +2,22 @@ from flask import request, make_response, session, abort
 from flask_restful import Resource
 
 from config import app, db, api
-from models import Show
+from models import Show, Blog
 
 
 class Home(Resource):
     def get(self):
         return 'how did i get here?'
+    
+class Shows(Resource):
+    def get(self):
+        shows = [s.to_dict() for s in Show.query.all()]
+
+        response = make_response(shows, 200)
+        return response
 
 api.add_resource(Home, '/')
+api.add_resource(Shows, '/shows')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
