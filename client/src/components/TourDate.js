0a0b88a5374ae0853f988feb678f16326fb2c008
Andre Vargas Roo <a.vargas252@icloud.com>
2023-07-16 11:04:49 -0400

    formatted date

--- client/src/components/TourDate.js
+++ client/src/components/TourDate.js
@@ -1,9 +1,14 @@
+import { format, parseISO } from 'date-fns' 
+
 const TourDate = ({venue, date, location, link}) => {
+    const parsedDate = parseISO(date)
+    const formattedDate = format(parsedDate, 'MMM dd, yyyy');
+
     return (
         <div className=
             'flex justify-between flex-row mx-10 md:mx-28 border-b border-stone-400 p-4 text-sm md:text-xl'
             >
-            <p>{date}</p>
+            <p>{formattedDate}</p>
             <p>{venue}</p>
             <p>{location}</p>
             <a href={link}>Buy Tickets</a>
