d45e196c4ee2dea8b01af460535b8e21988bb849
Andre Vargas Roo <a.vargas252@icloud.com>
2023-07-16 11:00:43 -0400

    added venue and changed button to a tag, installed date-fns

--- client/src/components/TourDate.js
+++ client/src/components/TourDate.js
@@ -1,11 +1,12 @@
-const TourDate = ({date, location, link}) => {
+const TourDate = ({venue, date, location, link}) => {
     return (
         <div className=
             'flex justify-between flex-row mx-10 md:mx-28 border-b border-stone-400 p-4 text-sm md:text-xl'
             >
             <p>{date}</p>
+            <p>{venue}</p>
             <p>{location}</p>
-            <button>{link}</button>
+            <a href={link}>Buy Tickets</a>
         </div>
     )
 }
