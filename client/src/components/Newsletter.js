c02976455969f0c8489bf1f267cc663361a7ff3d
Andre Vargas Roo <a.vargas252@icloud.com>
2023-07-25 22:32:57 -0400

    implementing Yup library

--- client/src/components/Newsletter.js
+++ client/src/components/Newsletter.js
@@ -1,28 +1,19 @@
 import { useEffect, useState } from "react"
 import {useFormik} from "formik"
+import * as Yup from "yup"
 
 const Newsletter = ({isOpen, handleNewsletter}) => {
     const [queue, setQueue] = useState([])
 
     useEffect(() => console.log(queue), [queue])
 
-    const validate = values => {
-        const errors = {}
-
-        if (!values.email) {
-          errors.email = 'Required'
-        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-          errors.email = 'Invalid email address'
-        }
-      
-        return errors
-    }
-
     const formik = useFormik({
         initialValues: {
             email: '',
         },
-        validate,
+        validationSchema: Yup.object({
+            email: Yup.string().email('Invalid email address').required('Required'),
+          }),
         onSubmit: values => {
             setQueue([...queue, values])
           },
