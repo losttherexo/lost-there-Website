8bb792afb7378b3f9f1c3f700884bb556e2c321e
Andre Vargas Roo <a.vargas252@icloud.com>
2023-07-25 22:06:20 -0400

    formik implementing minimally

--- client/src/components/Newsletter.js
+++ client/src/components/Newsletter.js
@@ -1,24 +1,21 @@
 import { useEffect, useState } from "react"
+import {useFormik} from "formik"
 
 const Newsletter = ({isOpen, handleNewsletter}) => {
-    const [email, setEmail] = useState('')
-    const [consent, setConsent] = useState(false)
-    const [queue, setQueue] = useState([])
+    // const [queue, setQueue] = useState([])
 
-    useEffect(() => console.log(queue), [queue])
+    // useEffect(() => console.log(queue), [queue])
 
-    const handleChange = (e) => {
-        setEmail(e.target.value)
-    }
-    
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        (consent ? setQueue(e => [...e, email]) : null)
-        setQueue(e => [...e, email])
-        setEmail('')
-        setConsent(false)
-        handleNewsletter()
-    }
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+          },
+    })
+
+
 
     if(!isOpen) {
         return null
@@ -26,13 +23,13 @@ const Newsletter = ({isOpen, handleNewsletter}) => {
 
     return (
         <div className=''>
-            <form onSubmit={handleSubmit} className='flex flex-col'>
+            <form onSubmit={formik.handleSubmit} className='flex flex-col'>
                 <button onClick={() => handleNewsletter()} className='absolute top-0 right-0 mr-2'>x</button>
                 <label htmlFor='email' className='font-medium'>Email</label>
-                <input placeholder='Type your email' name='email' onChange={handleChange} className='my-2 border rounded-sm' />
+                <input placeholder='Type your email' name='email' onChange={formik.handleChange} value={formik.values.email} className='my-2 border rounded-sm' />
                 <p className='font-medium'>I consent to being contacted by email</p>
                 <div className=''>
-                    <input onClick={() => setConsent(!consent)} type='checkbox' name='consent' />
+                    <input onClick={() => {}} type='checkbox' name='consent' />
                     <label htmlFor='consent' className='px-2'>Yes</label>
                 </div>
                 <button type='submit' className='bg-lime-500 rounded-md my-2 max-w-[25%] px-2 self-center'>Submit</button>
