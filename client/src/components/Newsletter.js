36915debfe8d96187ab62da523412a16439916f1
Andre Vargas Roo <a.vargas252@icloud.com>
2023-07-25 22:52:34 -0400

    cleanup of errors

--- client/src/components/Newsletter.js
+++ client/src/components/Newsletter.js
@@ -13,7 +13,8 @@ const Newsletter = ({isOpen, handleNewsletter}) => {
             consent: false,
         },
         validationSchema: Yup.object({
-            email: Yup.string().email('Invalid email address').required('Required'),
+            email: Yup.string().email('Invalid email address.').required('This field is required.'),
+            consent: Yup.boolean().oneOf([true], "Please select an option."),
           }),
         onSubmit: values => {
             setQueue([...queue, values])
@@ -31,13 +32,16 @@ const Newsletter = ({isOpen, handleNewsletter}) => {
                 <label htmlFor='email' className='font-medium'>Email</label>
                 <input placeholder='Type your email' name='email' onChange={formik.handleChange} value={formik.values.email} onBlur={formik.handleBlur} className='my-2 border rounded-sm' />
                 {formik.touched.email && formik.errors.email ? (
-                    <div>{formik.errors.email}</div>
+                    <div className="text-red-500">{formik.errors.email}</div>
                 ) : null}
                 <p className='font-medium'>I consent to being contacted by email</p>
                 <div className=''>
                     <input onClick={formik.handleChange} type='checkbox' name='consent' value={formik.values.consent} />
                     <label htmlFor='consent' className='px-2'>Yes</label>
                 </div>
+                {formik.touched.consent && formik.errors.consent ? (
+                    <div className="text-red-500">{formik.errors.consent}</div>
+                ) : null}
                 <button type='submit' className='bg-lime-500 rounded-md my-2 max-w-[25%] px-2 self-center'>Submit</button>
             </form>
         </div>
