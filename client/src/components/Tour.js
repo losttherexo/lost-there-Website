410d8270f088b000dfc1b535bcbcbd4e4d65efd3
Andre Vargas Roo <a.vargas252@icloud.com>
2023-07-31 23:44:50 -0400

    include error state for tour dates

--- client/src/components/Tour.js
+++ client/src/components/Tour.js
@@ -4,12 +4,16 @@ import axios from "axios"
 
 const Tour = () => {
     const [tourDates, setTourDates] = useState([])
+    const [error, setError] = useState(false)
 
     useEffect(() => {
         axios
             .get('/shows')
             .then(response => {setTourDates(response.data)})
-            .catch(error => {console.error("Error fetching tour dates:", error)})
+            .catch(error => {
+                console.error("Error fetching tour dates:", error)
+                setError(true)
+            })
     },[])
 
     const tourArray = tourDates.map(tourObj => <TourDate {...tourObj} key={tourObj.id}/>)
@@ -17,7 +21,7 @@ const Tour = () => {
     return (
         <div className='flex flex-col text-lime-500'>
             <h1 className='text-3xl font-bold p-4 self-center'>UPCOMING SHOWS</h1>
-            {tourArray}
+            {error? <h1 className='self-center mx-10 md:mx-28 p-4 text-sm md:text-xl'>I'm sorry, there has been an error :c</h1> : tourArray}
         </div>
     )
 }
