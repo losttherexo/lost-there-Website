4d87b7069a13393747260c419c225887aad3133c
Andre Vargas Roo <a.vargas252@icloud.com>
2023-07-31 23:05:06 -0400

    resizing bio div

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,9 +2,11 @@ import bio from '../data/bio.js'
 
 const About = () => {
     return (
-        <div>
-            <div className='flex justify-center py-3'>
-                <p className='text-lime-500'>{bio}</p>
+        <div className='flex justify-center h-screen'> 
+            <div className='w-[65%] md:w-[55%]'>
+                <div className='flex justify-center py-3'>
+                    <p className='text-lime-500 text-lg'>{bio}</p>
+                </div>
             </div>
         </div>
     )
