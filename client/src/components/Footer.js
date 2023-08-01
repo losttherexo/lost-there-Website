95b5f29debd9e87c8c49e6547c786d553ffc2f58
Andre Vargas Roo <a.vargas252@icloud.com>
2023-07-31 23:25:27 -0400

    fix links

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,10 +4,10 @@ const Footer = () => {
     return (
         <footer>
             <div className='fixed bottom-0 w-full text-white text-xl flex justify-center py-3'>
-                <a className='mx-2' href='https://www.instagram.com/toast_/'><FaInstagram /></a>
-                <a className='mx-2' href='https://www.youtube.com/@toasttv5283'><FaYoutube /></a>
-                <a className='mx-2' href='https://open.spotify.com/artist/3Nz5FVEfruc2XuwrGdeHeC?si=VqaigcauRwyy8CdLz4zLwA'><FaSpotify /></a>
-                <a className='mx-2' href='https://music.apple.com/us/artist/toast/1462028509'><FaMusic /></a>
+                <a className='mx-2' href='https://www.instagram.com/lostthere.xo/'><FaInstagram /></a>
+                <a className='mx-2' href='https://www.youtube.com/@losttherexo'><FaYoutube /></a>
+                <a className='mx-2' href='https://open.spotify.com/artist/27dTdSe8fBtCAle3kuMaOB?si=Erbf1WdZRlWreNLSuR7LIA'><FaSpotify /></a>
+                <a className='mx-2' href='https://music.apple.com/us/artist/lost-there/1576234563'><FaMusic /></a>
             </div>
         </footer>
     )
