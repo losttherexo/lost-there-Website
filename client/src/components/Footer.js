3dc62969c68bce226bb579f08979c9f0e22b371e
Andre Vargas Roo <a.vargas252@icloud.com>
2023-07-31 23:16:33 -0400

    create footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,16 @@
+import {FaInstagram, FaYoutube, FaSpotify, FaMusic } from 'react-icons/fa'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className='text-xl flex justify-center py-3 sticky top-[100vh]'>
+                <a className='mx-2' href='https://www.instagram.com/toast_/'><FaInstagram /></a>
+                <a className='mx-2' href='https://www.youtube.com/@toasttv5283'><FaYoutube /></a>
+                <a className='mx-2' href='https://open.spotify.com/artist/3Nz5FVEfruc2XuwrGdeHeC?si=VqaigcauRwyy8CdLz4zLwA'><FaSpotify /></a>
+                <a className='mx-2' href='https://music.apple.com/us/artist/toast/1462028509'><FaMusic /></a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
