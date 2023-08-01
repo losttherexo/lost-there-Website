98c1d1b700764c8d04136971753f36b42749b2dc
Andre Vargas Roo <a.vargas252@icloud.com>
2023-07-31 22:56:01 -0400

    importing bio

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,8 +1,10 @@
+import bio from '../data/bio.js'
+
 const About = () => {
     return (
         <div>
             <div className='flex justify-center py-3'>
-                <p className='text-lime-500'>this is about me</p>
+                <p className='text-lime-500'>{bio}</p>
             </div>
         </div>
     )
